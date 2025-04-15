c830928d1b3f8f64acdd4b65a320bad3404ecea9
Anthony <thesuper32203@gmail.com>
2025-04-15 17:31:55 -0400

    display job postings

--- src/main/java/org/example/jobapp/api/AdzunaClient.java
+++ src/main/java/org/example/jobapp/api/AdzunaClient.java
@@ -1,45 +1,69 @@
 package org.example.jobapp.api;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.HttpURLConnection;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class AdzunaClient {
 
     private String URL;
-    private AdzunaApiKey ad;
+    public AdzunaApiKey ad = new AdzunaApiKey();
 
     //stores the url
     public AdzunaClient(String job){
-
         //replaces spaces with plus sign allowing ability to read white spaces
+        System.out.println(ad.getId()+" " + ad.getKey());
         job.replaceAll(" ", "+");
+        URL ="https://api.adzuna.com/v1/api/jobs/us/search/1?app_id="+ad.getId()+"&app_key="+ad.getKey()+"&results_per_page=5&title_only=cashier";
 
-        URL ="http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id={"
-                + ad.getId()+
-                "}&app_key={" +
-                ad.getKey() +
-                "}&results_per_page=20&what=" + job + "&content-type=application/json";
 
     }
-    public void displayJobListings(AdzunaClient url) {
+    public JSONArray getJobListings(AdzunaClient url) {
 
         try {
 
-            //fetch the api response based on api link
+            // 1. fetch the api response based on api link
             HttpURLConnection client = fetchApiResponse(url);
 
-            //read the response and convert store string type
-            String response = readApiResponse(apiConnection);
+            //check for response status
+            //200 means that the connection was a success
+            if(client.getResponseCode() != 200){
+                System.out.println("Error! Could not connect to api");
+                throw new RuntimeException();
+            }
+
+            // 2. read the response and convert store string type
+            String response = readApiResponse(client);
+
+            // 3. parse the string into a json object
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) parser.parse(response);
+
+            // 4. retrieve array of job listings
+            JSONArray jobListings = (JSONArray) jsonObject.get("results");
+
+            return jobListings;
 
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
+
+    }
+
+    public void displayListings(JSONArray jobArray){
+
+        for(int i = 0; i < jobArray.size(); i++){
+            JSONObject job = (JSONObject) jobArray.get(i);
+            System.out.println("Title " + (String) job.get("description").toString());
+            System.out.println("URL " + (String) job.get("redirect_url"));
+        }
     }
 
     public HttpURLConnection fetchApiResponse(AdzunaClient api){
@@ -67,19 +91,17 @@ public class AdzunaClient {
         try{
             //create a string builder to store the resulting json data
             StringBuilder resultJson = new StringBuilder();
-
             //create a scanner to read from the InputStream of the HttpURLConnection
             Scanner scan = new Scanner(api.getInputStream());
-
             //loop through each line in the response and append it to the StringBuilder
             while(scan.hasNext()){
                 //read and append the current line to the StringBuilder
                 resultJson.append(scan.nextLine());
             }
-
             //close the sacnner to release resources associate with it
             scan.close();
             return resultJson.toString();
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/org/example/jobapp/cli/JobAppCLI.java
+++ src/main/java/org/example/jobapp/cli/JobAppCLI.java
@@ -1,5 +1,8 @@
 package org.example.jobapp.cli;
 
+import org.example.jobapp.api.AdzunaClient;
+import org.json.simple.JSONArray;
+
 import java.util.Scanner;
 
 public class JobAppCLI
@@ -10,8 +13,11 @@ public class JobAppCLI
         try (Scanner scan = new Scanner(System.in)) {
             //prompts user to enter job name
             System.out.println("Enter job you are looking for");
-            String job = scan.nextLine();//stores job user is looking for
-
+            String job =  "cashier"; //scan.nextLine();//stores job user is looking for
+            AdzunaClient findJob = new AdzunaClient(job);
+            JSONArray listing = new JSONArray();
+            listing = findJob.getJobListings(findJob);
+            findJob.displayListings(listing);
         }
 
     }
