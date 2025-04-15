190384262e630eef378f0d894de4ae7a959026e5
Anthony <thesuper32203@gmail.com>
2025-04-15 13:42:30 -0400

    AdzunaClient constructor to store api call

--- src/main/java/org/example/jobapp/api/AdzunaClient.java
+++ src/main/java/org/example/jobapp/api/AdzunaClient.java
@@ -9,10 +9,20 @@ import java.net.http.HttpResponse;
 public class AdzunaClient {
 
     private String URL;
+    private AdzunaApiKey ad;
 
+    //stores the url
     public AdzunaClient(String job){
-        URL = "http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=" +
-                {YOUR API ID}&app_key={YOUR API KEY}&results_per_page=20&what=javascript%20developer&content-type=application/json"
+
+        //replaces spaces with plus sign allowing ability to read white spaces
+        job.replaceAll(" ", "+");
+
+        URL ="http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id={"
+                + ad.getId()+
+                "}&app_key={" +
+                ad.getKey() +
+                "}&results_per_page=20&what=" + job + "&content-type=application/json";
+
     }
     public void displayJobListings() {
 
